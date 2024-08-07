5b17ea4e5301250275186897d73d0646f6b6149f
Naman joshi <namanjoc9761@gmail.com>
2024-08-07 15:34:32 +0530

    production fix

--- app/(routes)/workspace/_components/Editor.tsx
+++ app/(routes)/workspace/_components/Editor.tsx
@@ -1,127 +1,122 @@
-"use client"
-import React, { useEffect, useRef, useState } from 'react'
-import EditorJS, { API, ToolConstructable, OutputData } from "@editorjs/editorjs";
-
-// @ts-ignore
-import Header from '@editorjs/header';
-// @ts-ignore
+"use client";
+import React, { useEffect, useRef, useState } from "react";
+import EditorJS, { API, BlockToolConstructable, ToolConstructable, OutputData } from "@editorjs/editorjs";
+// Import the tools with their types
+import Header from "@editorjs/header";
 import List from "@editorjs/list";
-// @ts-ignore
-import Checklist from '@editorjs/checklist'
-// @ts-ignore
-import Paragraph from '@editorjs/paragraph';
-// @ts-ignore
-import Warning from '@editorjs/warning';
-import { useMutation } from 'convex/react';
-import { api } from '@/convex/_generated/api';
-import { toast } from 'sonner';
-import { FILE } from '../../dashboard/_components/FileList';
+import Checklist from "@editorjs/checklist";
+import Paragraph from "@editorjs/paragraph";
+import Warning from "@editorjs/warning";
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { toast } from "sonner";
+import { FILE } from "../../dashboard/_components/FileList";
 
 const rawDocument: OutputData = {
-    time: 1550476186479,
-    blocks: [
-      {
-        data: {
-          text: "Document Name",
-          level: 2,
-        },
-        id: "123",
-        type: "header",
+  time: 1550476186479,
+  blocks: [
+    {
+      data: {
+        text: "Document Name",
+        level: 2,
       },
-      {
-        data: {
-          level: 4,
-        },
-        id: "1234",
-        type: "header",
+      id: "123",
+      type: "header",
+    },
+    {
+      data: {
+        level: 4,
       },
-    ],
-    version: "2.8.1",
+      id: "1234",
+      type: "header",
+    },
+  ],
+  version: "2.8.1",
+};
+
+function Editor({
+  onSaveTrigger,
+  fileId,
+  fileData,
+}: {
+  onSaveTrigger: any;
+  fileId: any;
+  fileData: FILE;
+}) {
+  const ref = useRef<EditorJS>();
+  const updateDocument = useMutation(api.files.updateDocument);
+  const [document, setDocument] = useState<OutputData>(rawDocument);
+
+  useEffect(() => {
+    if (fileData) {
+      initEditor();
+    }
+  }, [fileData]);
+
+  useEffect(() => {
+    if (onSaveTrigger) {
+      onSaveDocument();
+    }
+  }, [onSaveTrigger]);
+
+  const initEditor = () => {
+    const editor = new EditorJS({
+      holder: "editorjs",
+      data: fileData?.document ? JSON.parse(fileData.document) : rawDocument,
+      tools: {
+        header: {
+          class: Header as unknown as ToolConstructable,
+          shortcut: "CMD+SHIFT+H",
+          config: {
+            placeholder: "Enter a Header",
+          },
+        },
+        list: {
+          class: List as unknown as ToolConstructable,
+          inlineToolbar: true,
+          config: {
+            defaultStyle: "unordered",
+          },
+        },
+        checklist: {
+          class: Checklist as unknown as ToolConstructable,
+          inlineToolbar: true,
+        },
+        paragraph: Paragraph as unknown as ToolConstructable,
+        warning: Warning as unknown as ToolConstructable,
+      },
+    });
+    ref.current = editor;
   };
-  
-  function Editor({
-    onSaveTrigger,
-    fileId,
-    fileData,
-  }: {
-    onSaveTrigger: any;
-    fileId: any;
-    fileData: FILE;
-  }) {
-    const ref = useRef<EditorJS>();
-    const updateDocument = useMutation(api.files.updateDocument);
-    const [document, setDocument] = useState<OutputData>(rawDocument);
-  
-    useEffect(() => {
-      if (fileData) {
-        initEditor();
-      }
-    }, [fileData]);
-  
-    useEffect(() => {
-      if (onSaveTrigger) {
-        onSaveDocument();
-      }
-    }, [onSaveTrigger]);
-  
-    const initEditor = () => {
-      const editor = new EditorJS({
-        holder: "editorjs",
-        data: fileData?.document ? JSON.parse(fileData.document) : rawDocument,
-        tools: {
-          header: {
-            class: Header as unknown as ToolConstructable,
-            shortcut: "CMD+SHIFT+H",
-            config: {
-              placeholder: "Enter a Header",
+
+  const onSaveDocument = () => {
+    if (ref.current) {
+      ref.current
+        .save()
+        .then((outputData) => {
+          updateDocument({
+            _id: fileId,
+            document: JSON.stringify(outputData),
+          }).then(
+            () => {
+              toast("Document Updated!");
             },
-          },
-          list: {
-            class: List as unknown as ToolConstructable,
-            inlineToolbar: true,
-            config: {
-              defaultStyle: "unordered",
-            },
-          },
-          checklist: {
-            class: Checklist as unknown as ToolConstructable,
-            inlineToolbar: true,
-          },
-          paragraph: Paragraph as unknown as ToolConstructable,
-          warning: Warning as unknown as ToolConstructable,
-        },
-      });
-      ref.current = editor;
-    };
-  
-    const onSaveDocument = () => {
-      if (ref.current) {
-        ref.current
-          .save()
-          .then((outputData) => {
-            updateDocument({
-              _id: fileId,
-              document: JSON.stringify(outputData),
-            }).then(
-              () => {
-                toast("Document Updated!");
-              },
-              () => {
-                toast("Server Error!");
-              }
-            );
-          })
-          .catch((error) => {
-            console.log("Saving failed: ", error);
-          });
-      }
-    };
-  
-    return (
-      <div>
-        <div id="editorjs" className="ml-20"></div>
-      </div>
-    );
-  }
-  
-  export default Editor;
+            () => {
+              toast("Server Error!");
+            }
+          );
+        })
+        .catch((error) => {
+          console.log("Saving failed: ", error);
+        });
+    }
+  };
+
+  return (
+    <div>
+      <div id="editorjs" className="ml-20"></div>
+    </div>
+  );
+}
+
+export default Editor;
